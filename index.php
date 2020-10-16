f36db0a38a713910aa748fc9dbeb00ba582248a5
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-17 00:04:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ShopQuest</title>
+    <title>Ka-nna-da</title>
   
     <!--bootstrap link-->
     <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
@@ -29,7 +29,7 @@
 <body>
     
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <a href="#" class="navbar-brand">ShopShop</a>
+    <a href="#" class="navbar-brand">Ka-nna-da</a>
     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
     </button>
