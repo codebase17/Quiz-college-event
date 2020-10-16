3cd3ac02275a83bf8244289b95089cbab1398c5b
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-16 23:49:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
 <?php?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>ka-nna-da</title>
+  <meta name="description" content="ka-nna-da">
+  <meta name="author" content="ka-nna-da">
+  
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a href="#" class="navbar-brand">ShopShop</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav">
+            <a href="#" class="nav-item nav-link active">Home</a>
+            <a href="about.php" class="nav-item nav-link">About</a>
+        </div>
+        <div class="navbar-nav ml-auto">
+            <?php if(isset($_SESSION['user_name'])) {
+                      
+                }
+                else{
+                    echo '<a href="register/register.php" class="nav-item nav-link">Register</a>
+                            <a href="login/login.php" class="nav-item nav-link">Login</a>&nbsp';
+                }
+            ?>
+        </div>
+    </div>
+</nav>
+  
+</body>
+</html>
