96e2bb550c8829f5bf5461bb646998a2d8bf9d5f
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-17 00:22:49 +0530

    Update login.php

--- login/login.php
+++ login/login.php
@@ -3,24 +3,22 @@
 ?>
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <a href="#" class="navbar-brand">Ka-nna-da</a>
+    <a href="../index.php" class="navbar-brand">Ka-nna-da</a>
     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav">
-            <a href="#" class="nav-item nav-link active">Home</a>
-            <a href="about.php" class="nav-item nav-link">About</a>
+            <a href="../index.php" class="nav-item nav-link active">Home</a>
+            <a href="../about.php" class="nav-item nav-link">About</a>
         </div>
         <div class="navbar-nav ml-auto">
-            <!--<a href="register/register.php" class="nav-item nav-link">Register</a>
-            <a href="login/login.php" class="nav-item nav-link">Login</a>&nbsp;&nbsp;-->
             <?php if(isset($_SESSION['user_name'])) {
   
                 }
                 else{
                     echo '<a href="register/register.php" class="nav-item nav-link">Register</a>
-                            <a href="login/login.php" class="nav-item nav-link">Login</a>';
+                            <a href="#" class="nav-item nav-link">Login</a>';
                 }
             ?>
       </div>
