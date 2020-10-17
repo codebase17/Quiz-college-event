f8d80c55b71b7eba7883837a5f0bfa210a35008c
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-17 21:15:28 +0530

    Update adding_question.php

--- pages/adding_question.php
+++ pages/adding_question.php
@@ -8,16 +8,22 @@
     $question=$POST['question'];
     $answer=$POST['answer'];
   
+
+    echo $ques_no."<br>";
+    echo $question."<br>";
+    echo $answer."<br>";
+    
+    
    if ($_SESSION['user_name']=="groot") {
     if (isset($_POST['add_question'])) {
     
       if(($con->query("insert into quiz(ques_no,question,answer) values('".mysqli_real_escape_string($con,$ques_no)."','".mysqli_real_escape_string($con,$question)."','".mysqli_real_escape_string($con,$answer)."')"))===True){
-                header("Location:added_question.php?added=yes");
-                die();
+                //header("Location:added_question.php?added=yes");
+                //die();
         }
         else{
-                header("Location:added_question.php?added=no");
-                echo $con->error;
+                //header("Location:added_question.php?added=no");
+                //echo $con->error;
         }
     }
     }
