061bb0e0656e939ead21801b64d9afdbe2af36ed
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-17 20:12:59 +0530

    Update header.php

--- header.php
+++ header.php
@@ -26,3 +26,9 @@
     <!-- Compiled and minified JavaScript -->
     <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>-->
 </head>
+
+    
+    <?php
+        include 'libraries/essentials.php';
+        session_start();
+    ?>
