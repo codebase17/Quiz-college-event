230cfe4a068225b87b6b45a4b6e4a6a947bc160e
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-19 00:29:46 +0530

    Update check_answer.php

--- pages/check_answer.php
+++ pages/check_answer.php
@@ -23,14 +23,36 @@
       $con->query("update matches set status='1',end_time='$cur' where user_name='$user' and quiz_no='$ques_no'");
       
       $prev = $con->query("select start_time from matches where user_name='$user' and quiz_no='$num'")->fetch_assoc()['start_time'];
+      
+	  echo "before string to time<br>";
+	  var_dump($prev);
+		echo "  ";
+	  var_dump($cur);
+	  
       $prev=strtotime($prev);
       $cur =strtotime($cur);
+	  
+	  echo "<br>before string to time<br>";
+	  var_dump($prev);
+		echo "  ";
+	  var_dump($cur);
+	 
       $prev = new DateTime("@$prev");
       $cur = new DateTime("@$cur");
+	  
+	  echo "<br>new Date string to time<br>";
+	  var_dump($prev);
+		echo "  ";
+	  var_dump($cur);
+	  
       $diff = $cur->getTimestamp() - $prev->getTimestamp();
+	  
+	  echo "<br>diff<br>";
+	  var_dump($diff);
+	  
       $con->query("update user set points=points+1,rank=rank+'$diff' where user_name='$user'");
-      header("Location:quiz.php?ans=correct");
-      die(); 
+      //header("Location:quiz.php?ans=correct");
+      //die(); 
   }
   else
   {
