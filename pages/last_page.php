06fb3af67f6a1fc226678218d051a10f65a83418
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-19 11:11:16 +0530

    Update last_page.php

--- pages/last_page.php
+++ pages/last_page.php
@@ -1,1 +1,61 @@
-<?php?>
+<?php
+
+    session_start();
+    include '../header.php';
+
+?>
+
+<body>
+    
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a href="../index.php" class="navbar-brand">Ka-nna-da</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav">
+            <a href="../index.php" class="nav-item nav-link">Home</a>
+	          <a href="leaderboard.php" class="nav-item nav-link active">Leaderboard</a>	
+            <a href="../about.php" class="nav-item nav-link">About</a>
+        </div>
+        <div class="navbar-nav ml-auto">
+            <!--<a href="register/register.php" class="nav-item nav-link">Register</a>
+            <a href="login/login.php" class="nav-item nav-link">Login</a>&nbsp;&nbsp;-->
+            <?php if(isset($_SESSION['user_name'])) {
+                        echo '<a href="../profile.php" class="nav-item nav-link active"><i class="fa fa-user-o">  '.$_SESSION['user_name'].'</i></a>';
+	 		                  echo '<a href="../login/logout.php" class="nav-item nav-link">Logout</a>';
+                }
+                else{
+                    echo '<a href="../register/register.php" class="nav-item nav-link">Register</a>
+                            <a href="../login/login.php" class="nav-item nav-link">Login</a>';
+                }
+            ?>
+      </div>
+    </div>
+</nav>
+  
+  <!--flag-->
+ 
+  <?php
+    
+    include '../flag.php';
+    
+   ?>
+  
+  <!--flag end-->
+  
+  
+  <div class="text-center">
+    <div class="container">
+      <h2>ಧನ್ಯವಾದಗಳು</h2>
+    </div>
+  </div>
+  
+ 
+  
+  
+  
+  
+  
+</body>
+</html>
