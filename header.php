65c5faac29da7e8a177b8ae5545774955cbd6a6f
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-26 17:36:13 +0530

    Update header.php

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ka-nna-da</title>
+    <title>ಅಲೆಮಾರಿ</title>
   
     <!--bootstrap link-->
     <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
