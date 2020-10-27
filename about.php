e0919add770819a595114871063fc19e0b709bf8
SSSShivani <42977733+SSSShivani@users.noreply.github.com>
2020-10-27 16:31:27 +0530

    Update about.php

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
 <body>
     
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <a href="index.php" class="navbar-brand">ಅಲೆಮಾರಿ</a>
+    <a href="index.php" class="navbar-brand">Quiz Application</a>
     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -19,8 +19,6 @@
             <a href="#" class="nav-item nav-link active">About</a>
         </div>
         <div class="navbar-nav ml-auto">
-            <!--<a href="register/register.php" class="nav-item nav-link">Register</a>
-            <a href="login/login.php" class="nav-item nav-link">Login</a>&nbsp;&nbsp;-->
             <?php if(isset($_SESSION['user_name'])) {
                         echo '<a href="profile.php" class="nav-item nav-link active"><i class="fa fa-user-o">  '.$_SESSION['user_name'].'</i></a>';
 	 		                  echo '<a href="login/logout.php" class="nav-item nav-link">Logout</a>';
@@ -32,28 +30,7 @@
             ?>
       </div>
     </div>
-</nav>
-  
-  
-  
-  <!--flag-->
- 
-  <?php
-    
-    include 'flag.php';
-    
-   ?>
-  
-  <!--flag end-->
-  
-  <div class="jumbotron text-center" >
-	  <p class="display-4 mb-2">ಅಲೆಮಾರಿ</p>
-	<p>ಯುವಿಸಿಇ ವೈಭವ ಪ್ರಯುಕ್ತ, IEEE UVCE ಕಡೆಯಿಂದ ನಿಧಿ ಶೋಧನೆ! ಕರುನಾಡ ಉದ್ದಗಲ ಪಯಣಿಸಿ, ಪ್ರಶ್ನೆಗಳಿಗೆ ಉತ್ತರಿಸಿ, ಮುಂದಿನ ನಿಲ್ದಾಣ ತಲುಪಿ!</p>
-	  
-	  <p class="display-4 mb-2 mt-4">Alemaari </p>
-	<p>The treasure hunt of UVCE Vaibhava presented to you by IEEE UVCE and Chetana. 
-	Answer the questions related to our state and find your way to top of the leaderboard.</p>
-  </div>
+	</nav>
   
 </body>
 </html>
