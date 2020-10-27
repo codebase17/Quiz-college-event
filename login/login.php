f9ffaa2df692f6b997f6ae048726d49896e84d36
SSSShivani <42977733+SSSShivani@users.noreply.github.com>
2020-10-27 17:09:23 +0530

    Update login.php

--- login/login.php
+++ login/login.php
@@ -3,7 +3,7 @@
 ?>
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <a href="../index.php" class="navbar-brand">ಅಲೆಮಾರಿ</a>
+    <a href="../index.php" class="navbar-brand">Quiz Application</a>
     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -27,14 +27,13 @@
 </nav>
   
 
-<!--flag-->
+
   
   <?php
-  include '../flag.php';
   $wrongpassword=$_GET['wrongpassword'];
 ?>
   
-  <!--flag end-->
+
 
 
 <div class="jumbotron p-4">
